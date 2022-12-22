164f33999b1ea4a6ceabce557f1c60ca0411ea9e
Alexandr Mityushkin <alexandrmityushkin@MacBook-Pro-Alexandr.local>
2022-12-22 21:00:06 +0300

    refactor: gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -7,6 +7,6 @@ OUTPUT_TASK = 'Find the greatest common divisor of given numbers.'
 def get_game():
     first_number = randint(1, 999)
     second_number = randint(1, 999)
-    correct_answer: str = gcd(first_number, second_number)
+    correct_answer: int | str = gcd(first_number, second_number)
     question: str = f'Question: {first_number} {second_number}'
     return question, str(correct_answer)
